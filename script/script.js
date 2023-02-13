6aebc744d0f49b60c47ea3e60b7f0d739732f685
Sadik <sadik.al.sami.2002@gmail.com>
2023-02-14 03:17:12 +0600

    fixed line spacing, fixed active states for bold,italic,underline

--- script/script.js
+++ script/script.js
@@ -8,29 +8,52 @@ const centerAlignBtn = document.getElementById("centerAlign");
 const justifyAlignBtn = document.getElementById("justifyAlign");
 const fontSizeInput = document.getElementById("fontSize");
 const myPara = document.getElementById("myPara");
+const boldIcon = document.getElementById("boldIcon");
+const italicIcon = document.getElementById("italicIcon");
+const underlineIcon = document.getElementById("underlineIcon");
 
 boldBtn.addEventListener("click", function () {
     if (myPara.style.fontWeight === "bold") {
         myPara.style.fontWeight = "normal";
+        boldBtn.classList.remove("bg-white");
+        boldIcon.classList.remove("text-blue-400");
     }
     else {
         myPara.style.fontWeight = "bold";
+        boldBtn.classList.add("bg-white");
+        boldIcon.classList.add("text-blue-400")
     }
 });
 italicBtn.addEventListener("click", function () {
     if (myPara.style.fontStyle === "italic") {
         myPara.style.fontStyle = "normal";
+        italicBtn.classList.remove("bg-white");
+        italicIcon.classList.remove("text-blue-400");
     } 
     else {
         myPara.style.fontStyle = "italic";
+        italicBtn.classList.add("bg-white");
+        italicIcon.classList.add("text-blue-400");
     }
 });
 underlineBtn.addEventListener("click", function () {
     if (myPara.style.textDecoration === "underline") {
         myPara.style.textDecoration = "none";
+        underlineBtn.classList.remove("bg-white");
+        underlineIcon.classList.remove("text-blue-400");
     }
     else {
         myPara.style.textDecoration = "underline"
+        underlineBtn.classList.add("bg-white");
+        underlineIcon.classList.add("text-blue-400");
+    }
+})
+justifyAlignBtn.addEventListener("click", function () {
+    if (myPara.style.textAlign === "justify") {
+        myPara.style.textAlign = "normal";
+    }
+    else {
+        myPara.style.textAlign = "justify"
     }
 })
 colorPicker.addEventListener("change", function () {
